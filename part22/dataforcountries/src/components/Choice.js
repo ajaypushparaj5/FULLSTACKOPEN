66a850f25b17bfbf0c3d2905cb35d13648173545
ajaypushparaj5 <23it012ajay@ug.cusat.ac.in>
2025-02-12 15:27:36 +0530

    flag beside country

--- part22/dataforcountries/src/components/Choice.js
+++ part22/dataforcountries/src/components/Choice.js
@@ -1,24 +1,33 @@
 import Results from './Results'
 import OneCountryDisplay from './OneCountryDisplay'
 
-const Choice = ({country, result}) => {
+const Choice = ({ country, result }) => {
+  let filtered = [];
 
-    let filtered = []
-
-    if(country.length > 0){
-        filtered = result.filter(result => 
-        result.name.common.toLowerCase().includes(country.toLowerCase()))
-    }else{
-        filtered = result
-    }
-
-    if(filtered.length > 10 ){
-      return ('Too many matches, specify another filter')
-    }else if (filtered.length === 1){
-      return (filtered.map(result => <OneCountryDisplay key={result.name.common} result={result}/>))
-    }else{
-      return (filtered.map(result => <Results key={result.name.common} result={result} />))
-    } 
+  if (country.length > 0) {
+    filtered = result.filter(res =>
+      res.name.common.toLowerCase().includes(country.toLowerCase())
+    );
+  } else {
+    filtered = result;
   }
 
-export default Choice
+  if (filtered.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  } else if (filtered.length === 1) {
+    return filtered.map(res => <OneCountryDisplay key={res.cca2} result={res} />);
+  } else {
+    return (
+      <div>
+        {filtered.map(res => (
+          <div key={res.cca2} style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <img src={res.flags.svg} alt={`Flag of ${res.name.common}`} width="40" />
+            <Results result={res} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default Choice;
